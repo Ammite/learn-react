a0f71f7bbec88fe68ee49205574aeb811be4d254
Ammite <baa21v@gmail.com>
2024-07-21 18:24:12 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function deleteTask(task) {
+  todoList = todoList.filter((item) => item !== task);
+  root.render(<MainView />);
+}
+
+function Task(attrs){
+  return (
+    <div className="task">
+      <li key="{attrs.task}">{attrs.task}</li>
+      <button className='deleteButton' onClick={() => deleteTask(attrs.task)}>Удалить</button>
+    </div>
+  );
+}
+
+function ToDoList() {
+  return (
+    <div id="list">
+      <h3>Всего задач: {todoList.length}</h3>
+      <ul>
+        {
+          todoList.map((task) => <Task task={task} />)
+        }
+      </ul>
+    </div>
+  );
+}
+
+function MainView() {
+  return (
+    <div id="mainView">
+      <h1>ToDo List</h1>
+      <div id="inputArea">
+        {inputArea}
+        {buttonAdd}
+      </div>
+      <ToDoList />
+    </div>
+  );
+}
+
+
+let todoList = [];
+const appDiv = document.getElementById("root");
+const root = ReactDOM.createRoot(appDiv);
+
+const addTask = () => {
+  const task = document.querySelector("input").value;
+  console.log("Added: " + task);
+  todoList.push(task);
+  root.render(<MainView />);
+};
+
+const buttonAdd = <button onClick={addTask}>Добавить</button>;
+const inputArea = <input placeholder="Введите задачу" />;
+
+root.render(<MainView />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
